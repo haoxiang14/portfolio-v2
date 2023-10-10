718b82137845f7722369adddaab2bf4979e52360
Tan Hao Xiang <Tp060906@mail.apu.edu.my>
2023-10-10 19:24:53 +0800

    Update Index page

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -2,7 +2,7 @@
     <div>
         <nav>
             <ul class="flex flex-col px-5 py-20">
-                <li class="mb-2 hover:bg-gray-200 px-16 py-2 rounded-md text-sm text-left"> <NuxtLink to="/home">Home</NuxtLink> </li>
+                <li class="mb-2 hover:bg-gray-200 px-16 py-2 rounded-md text-sm text-left"> <NuxtLink to="/">Home</NuxtLink> </li>
                 <li class="mb-2 hover:bg-gray-200 px-16 py-2 rounded-md text-sm text-left"> <NuxtLink to="/project">Project</NuxtLink> </li>
                 <li class="mb-2 hover:bg-gray-200 px-16 py-2 rounded-md text-sm text-left"> <NuxtLink to="/blog">Blog</NuxtLink> </li>
                 <li class="mb-2 hover:bg-gray-200 px-16 py-2 rounded-md text-sm text-left"> <NuxtLink to="/library"> My Library</NuxtLink> </li>
