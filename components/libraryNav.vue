870cd2ae3edc46173de7d4b451114090b40d1688
Tan Hao Xiang <Tp060906@mail.apu.edu.my>
2023-10-17 16:39:10 +0800

    Add library components

---
+++ components/libraryNav.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="px-16 py-4">
+        <ul class="flex">
+            <li class="hover:bg-gray-200 px-4 py-2 rounded-md text-left"> Articles </li>
+            <li class="hover:bg-gray-200 px-4 py-2 rounded-md text-left"> Twitter Threads </li>
+            <li class="hover:bg-gray-200 px-4 py-2 rounded-md text-left"> Books </li>
+            <li class="hover:bg-gray-200 px-4 py-2 rounded-md text-left"> YouTube Videos </li>
+        </ul>
+    </div>
+</template>
--- pages/library.vue
+++ pages/library.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="px-16 py-20">
+    <div class="px-16 pt-20">
         <h1 class="font-semibold text-2xl"> My Library </h1>
         <p class="font-normal text-xl py-6 text-gray-500"> Welcome to my Personal Library, a treasure trove of curated insights, comprising Twitter threads, YouTube videos, articles, books, and more. Explore my collection and embark on a journey of intellectual enrichment and personal growth.</p>
-
     </div>
+    <LibraryNav/>
 </template>
 
 
