765fe1d3616247eab5537db56bb3a5b66372f4a1
Hao Xiang <haoxiang@Haos-MacBook-Air.local>
2024-06-18 17:48:57 +0800

    Improve Sidebar design

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -2,72 +2,40 @@
     <div class="hidden lg:block lg:min-h-screen font-medium px-4 lg:px-8 lg:py-20">
         <nav>
             <ul class="flex flex-col">
-                <li class="mb-2 hover:bg-gray-200 px-16 py-2 rounded-md text-lg text-left"> 
-                    <NuxtLink to="/" class="flex items-center">
-                        <svg style="min-width: 24px; min-height: 24px;" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-home"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
-                        <p class="px-4"> Home </p>
-                    </NuxtLink> 
-                </li>
-                <li class="mb-2 hover:bg-gray-200 px-16 py-2 rounded-md text-lg text-left"> 
-                    <NuxtLink to="/project" class="flex items-center">
-                        <svg style="min-width: 24px; min-height: 24px;" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-code"><polyline points="16 18 22 12 16 6"/><polyline points="8 6 2 12 8 18"/></svg>
-                        <p class="px-4"> Project </p>
+                <li v-for="(item, index) in items" :key="index" :class="['mb-2 hover:bg-gray-200 px-4 py-2 my-4 rounded-md', { 'bg-gray-200': selectedItem === index }]" @click="selectedItem = index">
+                    <NuxtLink :to="item.path" class="flex items-center">
+                        <div v-html="item.svg"></div>
+                        <p class="px-4"> {{ item.name }} </p>
                     </NuxtLink>
                 </li>
-                <li class="mb-2 hover:bg-gray-200 px-16 py-2 rounded-md text-lg text-left"> 
-                    <NuxtLink to="/resume" class="flex items-center">
-                        <svg style="min-width: 24px; min-height: 24px;" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
-                        <p class="px-4">Resume</p>
-                    </NuxtLink> 
-                </li>
-                <li class="mb-2 hover:bg-gray-200 px-16 py-2 rounded-md text-lg text-left"> 
-                    <NuxtLink to="/contact" class="flex items-center">
-                        <svg style="min-width: 24px; min-height: 24px;" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-user"><rect width="18" height="18" x="3" y="3" rx="2"/><circle cx="12" cy="10" r="3"/><path d="M7 21v-2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2"/></svg>
-                        <p class="px-4">Contact</p>
-                    </NuxtLink> 
+            </ul>
+        </nav>  
+    </div>
+    <div class="lg:hidden fixed bottom-4 inset-x-4 z-50 bg-gray-300 shadow-md rounded-full">
+        <nav class="flex justify-around items-center h-16">
+            <ul class="flex gap-12">
+                <li v-for="(item, index) in items" :key="index" :class="['hover:bg-gray-200 px-2 py-2 rounded-full text-lg text-left', { 'bg-gray-200': selectedItem === index }]" @click="selectedItem = index">
+                    <NuxtLink :to="item.path" class="flex items-center">
+                        <div v-html="item.svg"></div>
+                    </NuxtLink>
                 </li>
             </ul>
         </nav>
-        <!-- <div class="mb-2 bg-white px-16 py-2 rounded-md text-lg text-left">
-            <div class="flex items-center">
-                <div>
-                    <a href="mailto: haoxiangt.14@gmail.com" target="_blank" class="flex items-center">
-                        <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://t.me/haoxiang_14" target="_blank" class="flex items-center">
-                        <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 496 512"><path d="M248,8C111.033,8,0,119.033,0,256S111.033,504,248,504,496,392.967,496,256,384.967,8,248,8ZM362.952,176.66c-3.732,39.215-19.881,134.378-28.1,178.3-3.476,18.584-10.322,24.816-16.948,25.425-14.4,1.326-25.338-9.517-39.287-18.661-21.827-14.308-34.158-23.215-55.346-37.177-24.485-16.135-8.612-25,5.342-39.5,3.652-3.793,67.107-61.51,68.335-66.746.153-.655.3-3.1-1.154-4.384s-3.59-.849-5.135-.5q-3.283.746-104.608,69.142-14.845,10.194-26.894,9.934c-8.855-.191-25.888-5.006-38.551-9.123-15.531-5.048-27.875-7.717-26.8-16.291q.84-6.7,18.45-13.7,108.446-47.248,144.628-62.3c68.872-28.647,83.183-33.623,92.511-33.789,2.052-.034,6.639.474,9.61,2.885a10.452,10.452,0,0,1,3.53,6.716A43.765,43.765,0,0,1,362.952,176.66Z"/></svg>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://twitter.com/haoxiang_14" target="_blank" class="flex items-center">
-                        <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm297.1 84L257.3 234.6 379.4 396H283.8L209 298.1 123.3 396H75.8l111-126.9L69.7 116h98l67.7 89.5L313.6 116h47.5zM323.3 367.6L153.4 142.9H125.1L296.9 367.6h26.3z"/></svg>
-                    </a>
-                </div>
-                <div>
-                    <a href="https://www.linkedin.com/in/haoxiang14/" target="_blank" class="flex items-center">
-                        <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
-                    </a>
-                </div>
-            </div>
-            <p class="text-sm"> Made with 💖 by haoxiang_14 </p>
-        </div> -->
-    </div>
-    <div class="lg:hidden fixed bottom-4 inset-x-4 z-50 bg-gray-300 shadow-md rounded-full">
-        <nav class="flex justify-around items-center h-20">
-            <NuxtLink to="/" class="text-center text-lg font-bold">
-                <span class="block"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-home"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg></span>
-            </NuxtLink>
-            <NuxtLink to="/project" class="text-center text-lg font-bold">
-                <span class="block"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-code"><polyline points="16 18 22 12 16 6"/><polyline points="8 6 2 12 8 18"/></svg></span>
-            </NuxtLink>
-            <NuxtLink to="/resume" class="text-center text-lg font-bold">
-                <span class="block"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg></span>
-            </NuxtLink>
-            <NuxtLink to="/contact" class="text-center text-lg font-bold">
-                <span class="block"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-user"><rect width="18" height="18" x="3" y="3" rx="2"/><circle cx="12" cy="10" r="3"/><path d="M7 21v-2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2"/></svg></span>
-            </NuxtLink>
-        </nav>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                selectedItem: null,
+                items: [
+                    { name: 'Home', path: '/', svg: '<svg style="min-width: 10px; min-height: 24px;" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-home"><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>' },
+                    { name: 'Project', path: '/project', svg: '<svg style="min-width: 24px; min-height: 24px;" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-code"><polyline points="16 18 22 12 16 6"/><polyline points="8 6 2 12 8 18"/></svg>' },
+                    { name: 'Resume', path: '/resume', svg: '<svg style="min-width: 24px; min-height: 24px;" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>'},
+                    { name: 'Contact', path: '/contact', svg: '<svg style="min-width: 24px; min-height: 24px;" width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-user"><rect width="18" height="18" x="3" y="3" rx="2"/><circle cx="12" cy="10" r="3"/><path d="M7 21v-2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v2"/></svg>' }
+                ]
+            }
+        }
+    }
+</script>
