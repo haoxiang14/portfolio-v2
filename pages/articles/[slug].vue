9f1e975915db78698c12495c488ecd663129d820
Hao Xiang <haoxiang@Haos-MacBook-Air.local>
2024-08-27 15:31:14 +0800

    Add article page

---
+++ pages/articles/[slug].vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="px-4 py-4 lg:px-20 lg:py-20">
+    <img class="w-full object-cover h-full" :src="article.image" alt="article-poster"/>
+    <article class="prose lg:prose lg:max-w-none">
+        <div class="flex flex-col items-center justify-center">
+          <h1 class="font-bold text-2xl lg:text-5xl pt-16"> {{ article.title }} </h1>
+          <div class="flex gap-4 text-gray-500">
+            <div class="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/></svg>
+              <p class="px-2"> {{ article.date }} </p>
+            </div>
+            <div class="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-hourglass"><path d="M5 22h14"/><path d="M5 2h14"/><path d="M17 22v-4.172a2 2 0 0 0-.586-1.414L12 12l-4.414 4.414A2 2 0 0 0 7 17.828V22"/><path d="M7 2v4.172a2 2 0 0 0 .586 1.414L12 12l4.414-4.414A2 2 0 0 0 17 6.172V2"/></svg>
+              <p class="px-2"> {{ article.readtime }} </p>
+            </div>  
+          </div>
+        </div>
+        
+        <ContentRendererMarkdown :value="article" />
+    </article>
+  </div>
+</template>
+
+<script setup>
+  
+  const route = useRoute()
+  const { data: article } = await useAsyncData('home', () => queryContent(`/articles/${route.params.slug}`).findOne())
+
+</script>
---
+++ pages/articles/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="px-4 py-4 lg:px-20 lg:py-20">
+        <h1 class="font-bold text-2xl lg:text-3xl"> Articles </h1>
+        <p class="font-normal lg:text-xl py-4 lg:py-8 text-gray-500"> Welcome to my cryptoverse, get ready to dive into the exciting realm of digital money, where I break down complex topics like blockchain and cryptocurrencies into simple, easy-to-understand pieces. Join me as we explore the latest trends, learn how this technology works, and discover how it's changing the way we think about money and the future of online transactions.</p>
+        <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            <div v-for="article in articles" :key="article.slug" class="py-2 lg:py-4">
+                <div class = "lg:p-8 p-4 border border-gray-200 rounded-t-xl flex items-center justify-center">
+                    <img class="border border-slate-200 rounded-xl" :src="article.image" alt="article-poster"/>
+                </div>
+                <div class="border border-gray-200 rounded-b-xl p-4 lg:p-10 lg:h-80 overflow-y-auto">
+                    <NuxtLink :to="`/articles/${article.slug}`" class="flex items-center">
+                        <h1 class="font-bold lg:text-3xl hover:underline hover:opacity-70"> {{ article.title }} </h1>
+                    </NuxtLink>
+                    <p class="font-normal text-xs lg:text-base text-gray-500 py-4 break-words"> {{ article.description }} </p>
+                    <div v-for="(type, index) in article.type" :key="index" class="inline-block">
+                        <span class="bg-blue-100 text-blue-500 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full"> {{ type }} </span>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const { data: articles } = await useAsyncData('home', () => queryContent('/articles').find());
+
+</script>
